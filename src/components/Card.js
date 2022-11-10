4490cf9756bbc8a44449a14f000eaa3fd60cb005
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-11-10 09:50:11 +0500

    Add animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@ import "./Card.css";
 import CardOne from "./CardOne";
 import CardThree from "./cardThree/CardThree";
 import CardTwo from "./CradTwo";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Card() {
   const card1 = useSelector((state) => state.moveCard.card1);
@@ -11,8 +12,25 @@ function Card() {
 
   return (
     <div className="mainContainer">
-      {card1 && <CardOne />}
-      {card2 && <CardTwo />}
+      <AnimatePresence>
+        {card1 && (
+          <motion.div
+            transition={{ duration: 0.3 }}
+            exit={{ x: "-100vw", opacity: 0.5 }}
+          >
+            <CardOne />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      {card2 && (
+        <motion.div
+          initial={{ x: "100vw", opacity: 0.5 }}
+          animate={{ x: 0, opacity: 0.7 }}
+          transition={{ duration: 0.3, delay: 0.5 }}
+        >
+          <CardTwo />
+        </motion.div>
+      )}
       {card3 && <CardThree />}
     </div>
   );
